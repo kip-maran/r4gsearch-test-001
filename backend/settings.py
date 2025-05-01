e4025e5df681cbf3af6435868f2a353822ea320d
KIP_MaRan <r-ma@kip.co.jp>
2025-05-01 10:08:03 +0900

    [upd]settings.py復元

--- backend/settings.py
+++ backend/settings.py
@@ -41,13 +41,13 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "R4G検索サービス(test002)"
+    title: str = "Contoso"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "対話をはじめよう"
-    chat_description: str = "R4Gドキュメント、リファレンス、CACBソースをベースに回答します。"
+    chat_title: str = "Start chatting"
+    chat_description: str = "This chatbot is configured to answer your questions"
     favicon: str = "/favicon.ico"
-    show_share_button: bool = False
+    show_share_button: bool = True
     show_chat_history_button: bool = True
 
 
